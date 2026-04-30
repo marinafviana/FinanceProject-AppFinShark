aeffc8823ff8c256681bf19fd232f6bb1c749079
marinafviana <marinafviana7@gmail.com>
2026-04-30 11:27:04 -0300

    Commit App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,22 +2,39 @@ import { ChangeEvent, SyntheticEvent, useState } from "react";
 import "./App.css";
 import CardList from "./Components/CardList/CardList";
 import Search from "./Components/Search/Search";
+import { searchCompanies } from "./api";
+import { CompanySearch } from "./company";
 
 function App() {
   const [search, setSearch] = useState<string>("");
-  
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-      setSearch(e.target.value);
-      console.log(e);
-    };
-  
-    const onClick = (e: SyntheticEvent) => {
-      console.log(e);
-    };
+  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
+  const [serverError, setServerError] = useState<string | null>(null);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onClick = async (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    const result = await searchCompanies(search);
+
+    if (typeof result === "string") {
+      setServerError(result);
+      setSearchResult([]);
+    } else {
+      setSearchResult(result); 
+      setServerError(null);
+    }
+  };
+
   return (
     <div className="App">
-      <Search onClick={onClick}  search={search} handleChange={handleChange} />
-      <CardList />
+      <Search onClick={onClick} search={search} handleChange={handleChange} />
+
+      {serverError && <div>Unable to connect to API</div>}
+
+      <CardList searchResult={searchResult} />
     </div>
   );
 }
