6298d463a062132face477a28a486758774cddff
marinafviana <marinafviana7@gmail.com>
2026-04-30 11:15:39 -0300

    commit de recuperacao de erro

---
+++ frontend/src/api.tsx
@@ -0,0 +1,42 @@
+import { ChangeEvent, SyntheticEvent, useState } from "react";
+import "./App.css";
+import CardList from "./Components/CardList/CardList";
+import Search from "./Components/Search/Search";
+import { searchCompanies } from "./api";
+import { CompanySearch } from "./company";
+
+function App() {
+  const [search, setSearch] = useState<string>("");
+  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
+  const [serverError, setServerError] = useState<string | null>(null);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onClick = async (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    const result = await searchCompanies(search);
+
+    if (typeof result === "string") {
+      setServerError(result);
+      setSearchResult([]);
+    } else {
+      setSearchResult(result); 
+      setServerError(null);
+    }
+  };
+
+  return (
+    <div className="App">
+      <Search onClick={onClick} search={search} handleChange={handleChange} />
+
+      {serverError && <p>{serverError}</p>}
+
+      <CardList searchResult={searchResult} />
+    </div>
+  );
+}
+
+export default App;
