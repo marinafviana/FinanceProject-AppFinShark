d07fb4cc91b3facda18ff545d03b8e051479c9cd
marinafviana <marinafviana7@gmail.com>
2026-05-13 17:07:43 -0300

    Commit POST(Create)

--- api/obj/Debug/net10.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4e305debb09463e380277a807d4c745faa24abc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a470e858019704e1fd96200ad106482277aaba7")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
