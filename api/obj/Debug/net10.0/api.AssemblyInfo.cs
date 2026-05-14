8aef28b78340e581e30f088ffcae70a53d009e6a
marinafviana <marinafviana7@gmail.com>
2026-05-14 00:31:14 -0300

    Commit Comment System

--- api/obj/Debug/net10.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+013c0fe5f32d5778358d036853365fd2485ad006")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b8ba661e1faeda2f66191d101612b2e57507465")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
