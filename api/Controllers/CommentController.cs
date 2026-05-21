84cc7812528fd4bad63c7332794aaf585ec83756
marinafviana <marinafviana7@gmail.com>
2026-05-21 10:03:21 -0300

    Commit Comment UPDATE

--- api/Controllers/CommentController.cs
+++ api/Controllers/CommentController.cs
@@ -63,4 +63,18 @@ namespace api.Controllers
                 commentModel.ToCommentDto());
         }
     }
+
+     [HttpPut]
+     [Route("{id}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateCommentRequestDto updateDto)
+        {
+            var comment = await _commentRepo.UpdateAsync(id, updateDto.ToCommentFromUpdate(id));
+
+            if (comment == null)
+            {
+                return NotFound("Comment not found");
+            }
+
+            return Ok(comment.ToCommentDto());
+        }
 }
--- api/Mappers/CommentMapper.cs
+++ api/Mappers/CommentMapper.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using api.Dtos.Comment;
 using api.Models;
-
+    
 namespace api.Mappers
 {
     public static class CommentMapper
@@ -22,6 +22,16 @@ namespace api.Mappers
         }
 
         public static Comment ToCommentFromCreate(this CreateCommentDto commentDto, int stockId)
+        {
+            return new Comment
+            {
+                Title = commentDto.Title,
+                Content = commentDto.Content,
+                StockId = stockId
+            };
+        }
+
+        public static Comment ToCommentFromUpdate(this UpdateCommentRequestDto commentDto, int stockId)
         {
             return new Comment
             {
