9bdd3c5180a3be6ab24d305d8e7afd4522561b25
marinafviana <marinafviana7@gmail.com>
2026-05-21 10:18:38 -0300

    Commit Comment Delete

--- api/Controllers/CommentController.cs
+++ api/Controllers/CommentController.cs
@@ -15,7 +15,6 @@ namespace api.Controllers
     {
         private readonly ICommentRepository _commentRepo;
         private readonly IStockRepository _stockRepo;
-
         public CommentController(ICommentRepository commentRepo, IStockRepository stockRepo)
         {
             _commentRepo = commentRepo;
@@ -46,7 +45,7 @@ namespace api.Controllers
         }
 
         [HttpPost("{stockId}")]
-        public async Task<IActionResult> Create([FromRoute] int stockId, [FromBody] CreateCommentDto commentDto)
+        public async Task<IActionResult> Create([FromRoute] int stockId, CreateCommentDto commentDto)
         {
             if (!await _stockRepo.StockExists(stockId))
             {
@@ -54,18 +53,12 @@ namespace api.Controllers
             }
 
             var commentModel = commentDto.ToCommentFromCreate(stockId);
-
             await _commentRepo.CreateAsync(commentModel);
-
-            return CreatedAtAction(
-                nameof(GetById),
-                new { id = commentModel.Id },
-                commentModel.ToCommentDto());
+            return CreatedAtAction(nameof(GetById), new { id = commentModel }, commentModel.ToCommentDto());
         }
-    }
 
-     [HttpPut]
-     [Route("{id}")]
+        [HttpPut]
+        [Route("{id}")]
         public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateCommentRequestDto updateDto)
         {
             var comment = await _commentRepo.UpdateAsync(id, updateDto.ToCommentFromUpdate(id));
@@ -77,4 +70,19 @@ namespace api.Controllers
 
             return Ok(comment.ToCommentDto());
         }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            var commentModel = await _commentRepo.DeleteAsync(id);
+
+            if (commentModel == null)
+            {
+                return NotFound("Comment does not exist");
+            }
+
+            return Ok(commentModel);
+        }
+    }
 }
