1807bfc90c4ccc98e4da6f138f89e4053ad4d62d
marinafviana <marinafviana7@gmail.com>
2026-06-04 12:18:33 -0300

    Commit React Context Auth JWT (Services)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return <>
     <Navbar />
     <Outlet />
+    <ToastContainer />
   </>;
 
 }
